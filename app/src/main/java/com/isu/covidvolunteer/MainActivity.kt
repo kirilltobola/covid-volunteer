696337ff34284bc0c4da8e9922cc4d493aaa34f8
Kirill Tobola <kirilltobola@gmail.com>
2022-03-24 03:37:01 +0800

    add gradle support files

--- app/src/main/java/com/isu/covidvolunteer/MainActivity.kt
+++ app/src/main/java/com/isu/covidvolunteer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.isu.covidvolunteer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.isu.covidvolunteer.models.auth.AuthDto
+import com.isu.covidvolunteer.util.Retrofit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
